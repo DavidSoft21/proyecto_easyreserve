afcb11282c6ed421db3e8ad4a545a9a8676dfcbd
cristian torres <ctorresolarte@gmail.com>
2021-10-11 19:32:51 -0500

    tabla

--- client/cli/src/views/Dashboard.vue
+++ client/cli/src/views/Dashboard.vue
@@ -4,6 +4,35 @@
     <b-button type="submit" variant="danger" @click="removerItem"
       >cerrar sesion</b-button
     >
+
+      <table class="table">
+        <thead>
+            <tr>
+                <!--<th scope="col">ID</th>
+                <th scope="col">TIPO HABITACION</th>
+                <th scope="col">DESCRIPCION</th>
+                <th scope="col">ACCION</th>-->
+
+                <th scope="col">ID</th>
+                <th scope="col">NOTAS</th>
+                <th scope="col">DESCRIPCION</th>
+                <th scope="col">ACCION</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(item, index) in notas" :key="index">
+                <th scope="row">{{item._id}}</th>
+                <td>{{item.tipo_habitacion}}</td>
+                <td>item.descripcion</td>
+
+                <td>
+
+                </td>
+            </tr>
+        </tbody>
+    </table>
+
+
   </div>
 </template>
 
